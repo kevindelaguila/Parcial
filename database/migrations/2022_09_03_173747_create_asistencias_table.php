682f915695c596e724f662008e4dd2b762c47741
kevindelaguila <kevindelaguila007@gmail.com>
2022-09-18 08:19:56 -0600

    parcial

---
+++ database/migrations/2022_09_03_173747_create_asistencias_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asistencias', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha');
+            $table->dateTime('entrada');
+            $table->dateTime('salida');
+            $table->time('horas')->nullable();
+            $table->timestamps();
+            $table->foreignId('id')->constrained();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asistencias');
+    }
+};
